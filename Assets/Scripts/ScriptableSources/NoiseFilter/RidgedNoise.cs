481538fb4e860c0756fd05ee211b9170b43e554f
Robin Stegemann <robinstegemann@hotmail.de>
2023-09-15 23:47:06 +0200

    Added RidgedNoise.cs, not working yet tho

---
+++ Assets/Scripts/ScriptableSources/NoiseFilter/RidgedNoise.cs
@@ -0,0 +1,66 @@
+﻿using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(menuName = "Noise/RidgedNoise")]
+public class RidgedNoise : NoiseFilter
+{
+    public float scale = 50;
+    public int octaves = 6; 
+    [Range(0f, 1f)]
+    public float persistence = 0.6f;
+    public float baseRoughness = 0;
+
+    public override void OnValidate()
+    {
+        base.OnValidate();
+        ValidateValues();
+    }
+    
+    public override float MinValue()
+    {
+        return 0;
+    }
+
+    public override float MaxValue()
+    {
+        float amplitude = 1;
+        float elevation = 0;
+        float weight = 1;
+        for (int i = 0; i < octaves; i++)
+        {
+            float v = 1;
+            v *= weight;
+            weight = v;
+            elevation = v * amplitude;
+            amplitude *= persistence;
+        }
+        return elevation;
+    }
+
+    public override float Evaluate(float x, float y, Vector2 sampleCenter)
+    {
+        float amplitude = 1;
+        float elevation = 0;
+        float weight = 1;
+        for (int i = 0; i < octaves; i++)
+        {
+            float v = 1 - Mathf.Sin(Mathf.PerlinNoise(x + sampleCenter.x + baseRoughness, y + sampleCenter.y + baseRoughness));
+            v *= v;
+            v *= weight;
+            weight = v;
+            elevation = v * amplitude;
+            amplitude *= persistence;
+        }
+
+        return elevation;
+    }
+    
+    public override void ValidateValues()
+    {
+        scale = Mathf.Max(scale, 0.01f);
+        octaves = Mathf.Max(octaves, 1);
+        persistence = Mathf.Clamp01(persistence);
+    }
+}
